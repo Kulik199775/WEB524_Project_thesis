1c03306b371cabf4c11e0c0177590f5582bc85c7
Dmitriy Kulikov <dimonkulikov@mail.ru>
2026-04-04 13:21:38 +0300

    1. Добавлена модель пользователя в админку; 2. Внесены необходимые изменения в настройки.

--- config/settings.py
+++ config/settings.py
@@ -133,7 +133,9 @@ AUTH_PASSWORD_VALIDATORS = [
 
 LANGUAGE_CODE = "ru-ru"
 
-TIME_ZONE = "UTC"
+TIME_ZONE = "Europe/Moscow"
+
+USE_L10N = True
 
 USE_I18N = True
 
@@ -153,7 +155,8 @@ STATIC_URL = "static/"
 # )
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
-# AUTH_USER_MODEL = 'users.User'
+AUTH_USER_MODEL = 'users.User'
+
 # LOGIN_REDIRECT_URL = "dogs:index"
 # LOGOUT_REDIRECT_URL = "dogs:index"
 # LOGIN_URL = 'users:user_login'

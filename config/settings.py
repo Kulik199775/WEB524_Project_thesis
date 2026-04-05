090a9115837df90c932c196deff9f6299f37c667
Dmitriy Kulikov <dimonkulikov@mail.ru>
2026-04-05 10:45:07 +0300

    1. Внесены изменения в настройки проекта. 2. Добавлен файл urls.py в приложение catalog. 3. Внесены изменения в urls проекта. 4. Добавлены товары с картинками.

--- config/settings.py
+++ config/settings.py
@@ -147,13 +147,13 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
 STATIC_URL = "static/"
-# STATICFILES_DIRS = (
-#     BASE_DIR / 'static',
-# )
-# MEDIA_URL = '/media/'
-# MEDIA_ROOT = (
-#     BASE_DIR / 'media'
-# )
+STATICFILES_DIRS = (
+    BASE_DIR / 'static',
+)
+MEDIA_URL = '/media/'
+MEDIA_ROOT = (
+    BASE_DIR / 'media'
+)
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 AUTH_USER_MODEL = 'users.User'

16ed77b73cf307f2417a05a522b38c9721f43d8c
Dmitriy Kulikov <dimonkulikov@mail.ru>
2026-04-06 20:26:40 +0300

    Обновлены urls проекта в части приложения reviews

--- config/urls.py
+++ config/urls.py
@@ -25,4 +25,5 @@ urlpatterns = [
                   path('', include('catalog.urls', namespace='catalog')),
                   path('users/', include('users.urls')),
                   path('cart/', include('cart.urls')),
+                  path('reviews/', include('reviews.urls')),
               ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
